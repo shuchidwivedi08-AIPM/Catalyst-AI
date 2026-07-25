c9259987e1b34fe3870e78da351f7692e00f6747
shuchidwivedi08-AIPM <shuchidwivedi08@gmail.com>
2026-07-25 12:08:11 +0530

    Export project services

---
+++ catalyst_ai/projects/__init__.py
@@ -0,0 +1,18 @@
+"""Project workspace package."""
+
+from catalyst_ai.projects.models import Project, ProjectMembership
+from catalyst_ai.projects.service import (
+    ProjectError,
+    create_project,
+    get_accessible_project,
+    list_user_projects,
+)
+
+__all__ = [
+    "Project",
+    "ProjectMembership",
+    "ProjectError",
+    "create_project",
+    "get_accessible_project",
+    "list_user_projects",
+]
